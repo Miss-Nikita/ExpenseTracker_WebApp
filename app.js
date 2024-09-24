16dbf9f848d4dd3f809079064a50e425bae517aa
Miss-Nikita <bisennikita770@gmail.com>
2024-09-24 17:54:31 +0530

    Connect-flash | Reset-Password

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const session = require("express-session");
 const UserSchema = require("./models/user.schema");
 
 
+// connect flash
+const flash = require("connect-flash")
+
+
 // db connection
 require("./config/db")
 
@@ -46,6 +50,10 @@ app.use(
       secret: process.env.EXPRESS_SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
+       cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 24,
+        },
   })
 );
 app.use(passport.initialize());
@@ -53,6 +61,8 @@ app.use(passport.session());
 passport.serializeUser(UserSchema.serializeUser());
 passport.deserializeUser(UserSchema.deserializeUser());
 
+
+app.use(flash());
 
 // base route
 app.use('/', indexRouter);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const UserSchema = require('../models/user.schema')
-const { isLoggedIn }  = require("../middlewares/auth.middleware")
+const { isLoggedIn } = require("../middlewares/auth.middleware")
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -16,7 +16,7 @@ router.get("/signup", async (req, res) => {
   });
 });
 
-router.post("/signup", async (req, res,next) => {
+router.post("/signup", async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     await UserSchema.register({ username, email }, password);
@@ -36,20 +36,36 @@ router.get("/signin", async (req, res) => {
 });
 
 
-router.post(
-  "/signin",
-  passport.authenticate("local", {
-    successRedirect: "/user/profile",
-    failureRedirect: "/user/signin",
-  }),
-  (req, res) => { }
-);
-
-router.get("/profile", isLoggedIn, async (req, res,next) => {
+router.post("/signin", passport.authenticate("local"), async (req, res) => {
   try {
+    req.flash("success", "Successfully LoggedIn!");
+    res.redirect("/user/profile");
+  } catch (error) {
+    console.log(error);
+    next(error);
+    // res.redirect("/user/signin");
+  }
+});
+
+
+// router.post(
+//   "/signin",
+//   passport.authenticate("local", {
+//     successRedirect: "/user/profile",
+//     failureRedirect: "/user/signin",
+//   }),
+//   (req, res) => { }
+// );
+
+router.get("/profile", isLoggedIn, async (req, res, next) => {
+  try {
+
+    const message = req.flash("success");
+
     res.render("profileuser", {
       title: "Expense Tracker | Profile",
       user: req.user,
+      message,
     });
   } catch (error) {
     next(error);
@@ -62,5 +78,62 @@ router.get("/signout", isLoggedIn, async (req, res) => {
   });
 });
 
+
+router.get("/reset-password", isLoggedIn, async (req, res) => {
+  res.render("resetpassworduser", {
+      title: "Expense Tracker | Reset Password",
+      user: req.user,
+  });
+});
+router.post("/reset-password", isLoggedIn, async (req, res) => {
+  try {
+      await req.user.changePassword(
+          req.body.oldpassword,
+          req.body.newpassword
+      );
+      await req.user.save();
+      res.redirect("/user/profile");
+  } catch (error) {
+      next(error);
+  }
+});
+
+
+router.get("/delete-account", isLoggedIn, async (req, res) => {
+  try {
+      await UserSchema.findByIdAndDelete(req.user._id);
+      // code to delete profile pic
+      // code to delete all relaated expenses
+      res.redirect("/user/signin");
+  } catch (error) {
+      next(error);
+  }
+});
+
+
+router.get("/update", isLoggedIn, async (req, res) => {
+  res.render("updateuser", {
+      title: "Expense Tracker | Update User",
+      user: req.user,
+  });
+});
+
+
+router.post("/update", isLoggedIn, async (req, res) => {
+  try {
+      await UserSchema.findByIdAndUpdate(req.user._id, req.body);
+      res.redirect("/user/profile");
+  } catch (error) {
+      next(error);
+  }
+});
+
+router.post("/avatar", isLoggedIn, async (req, res) => {
+  try {
+      // user multer and req.file.filename and update to user
+  } catch (error) {
+      next(error);
+  }
+});
 module.exports = router;
 
