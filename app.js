fce134e11113342567f8f062b8d7ab5e7d09402a
Miss-Nikita <bisennikita770@gmail.com>
2024-09-25 21:07:04 +0530

    GoogleOAuth | Nodemailer

--- app.js
+++ app.js
@@ -58,8 +58,13 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser(UserSchema.serializeUser());
-passport.deserializeUser(UserSchema.deserializeUser());
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 
 app.use(flash());
