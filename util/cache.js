03880cdddd6d634d06e3956d3cc9d01e63ed847b
Miss-Nikita <bisennikita770@gmail.com>
2024-12-13 20:19:53 +0530

    ..

--- util/cache.js
+++ util/cache.js
@@ -1,22 +1,22 @@
-const { createClient }  =  require('redis');
+// const { createClient }  =  require('redis');
 
-const client = createClient({
-    password: 'GUQ59GNqCpPqFldB1tGUT0zRC4t8eyeo',
-    socket: {
-      host: 'redis-13648.c84.us-east-1-2.ec2.redns.redis-cloud.com',
-      port: 13648
-  }
-});
+// const client = createClient({
+//     password: 'GUQ59GNqCpPqFldB1tGUT0zRC4t8eyeo',
+//     socket: {
+//       host: 'redis-13648.c84.us-east-1-2.ec2.redns.redis-cloud.com',
+//       port: 13648
+//   }
+// });
 
 
 
-client.on('connect', () => {
-    console.log('Connected to Redis');
-  });
+// client.on('connect', () => {
+//     console.log('Connected to Redis');
+//   });
 
-  client.on('error', (err) => {
-    console.error('Redis Client Error:', err);
-  });
+//   client.on('error', (err) => {
+//     console.error('Redis Client Error:', err);
+//   });
   
-client.connect()
-module.exports = client
+// client.connect()
+// module.exports = client
